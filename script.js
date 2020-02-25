62aeeff68064b7bffce0a04ff60691d1c9f06959
Jake Kile <jkile9@gmail.com>
2020-02-24 23:39:45 -0700

    switched urls to HTTPS

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ $("#searchButton").on("click", function (e) {
 })
 
 function currentWeatherCall(city) {
-    currentQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6133582a13f76cf0a556408e3196e907&units=imperial";
+    currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=6133582a13f76cf0a556408e3196e907&units=imperial";
     $.ajax({
         url: currentQueryURL,
         type: "GET"
@@ -66,14 +66,14 @@ function currentWeatherCall(city) {
         windSpeed = response.wind.speed;
         latitude = response.coord.lat;
         longitude = response.coord.lon;
-        let icon = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        let icon = "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
         uvCall(latitude, longitude, city, temp, humidity, windSpeed, icon);
     })
 }
 
 function uvCall(latitude, longitude, city, temp, humidity, windSpeed, icon) {
     $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/uvi?appid=6133582a13f76cf0a556408e3196e907&lat=" + latitude + "&lon=" + longitude,
+        url: "https://api.openweathermap.org/data/2.5/uvi?appid=6133582a13f76cf0a556408e3196e907&lat=" + latitude + "&lon=" + longitude,
         type: "GET"
     }).done(function (response) {
         uvIndex = response.value;
@@ -89,7 +89,7 @@ $("#menu").on("click", "button", function (e) {
 })
 
 function forcastWeatherCall(city) {
-    currentQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=6133582a13f76cf0a556408e3196e907&units=imperial";
+    currentQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=6133582a13f76cf0a556408e3196e907&units=imperial";
     $.ajax({
         url: currentQueryURL,
         type: "GET"
@@ -101,7 +101,7 @@ function forcastWeatherCall(city) {
             let currentDate = response.list[i].dt_txt.toString().slice(0, 10);
             let callDate = moment().format("YYYY") + "-" + moment().format("MM") + "-" + (parseInt(moment().format("D")) + iterator + 1);
             if (currentDate === callDate) {
-                let icon = "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png"
+                let icon = "https://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png"
                 let temp = response.list[i].main.temp;
                 let humidity = response.list[i].main.humidity;
                 forecastDisplay(temp, humidity, icon, iterator);
